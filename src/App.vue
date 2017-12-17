b682f10f44de5ecd3b9e2f465bf59a39012d5f28
carzy <394163807@qq.com>
2017-12-17 22:00:53 +0800

    add 路由

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,44 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">菜单2</a></li>
+      <li><a href="#">菜单3</a></li>
+      <li><a href="#">菜单4</a></li>
+      <li><a href="#">菜单5</a></li>
+    </ul>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
-}
+  name: "app"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+ul {
+  left: 0;
+}
+
+ul li {
+  list-style: none;
+  float: left;
+  margin: 5px;
+  background-color: #eeeeee;
+}
+
+ul li a{
+  text-decoration:none; 
 }
 </style>
---
+++ src/pages/demo1/Demo1.vue
@@ -0,0 +1,16 @@
+<<template>
+  <div>
+      <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'THIS IS Demo1'
+    }
+  }
+}
+</script>
