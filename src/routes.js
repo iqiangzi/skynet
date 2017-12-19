9cc4ad8934ba055212ede1b6909d890791638eab
carzy <394163807@qq.com>
2017-12-19 10:45:56 +0800

    新增 404.html

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Header from './components/header.vue'
-import Footer from './components/footer.vue'
-import Home from './pages/home/home.vue'
+import Home from './pages/home/home'
 import Charts from './pages/charts/charts'
-import Details from './pages/detail/details.vue'
+import Details from './pages/detail/details'
+import NotFoundComponent from "./pages/404";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {path: '/home', component: Home, name:'home',
-   children: [
-        {
-          path: 'footer',
-          component: Footer
-        }
-      ]
-  },
+  {path: '/home', component: Home, name:'home'},
   {path: '/details/:id', component: Details, name: 'details'},
   {path: '/charts', component: Charts, name:'charts'},
-  {path: '*', component: Home, name:'home'}
+  {path: '*', component: NotFoundComponent}
 ]
 
 const router = new VueRouter({
