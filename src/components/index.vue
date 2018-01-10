1bb66e991b65afc6a41f457eb30913356e7a6f01
yancary <duanjueailian@qq.com>
2018-01-10 15:04:30 +0800

    Update configuration

--- src/components/index.vue
+++ src/components/index.vue
@@ -5,11 +5,12 @@
 	<h2>系统正在升级中.....</h2>
 	<li>服务内容：</li>
 	<li>1.考试答案</li>
-	<li>2.在线作业答案</li>
+	<li>2.在线作业答案  加入我们的QQ群:</li>
+	<a target="_blank" href="//shang.qq.com/wpa/qunwpa?idkey=b9fb3593caf0772889bccf48a31fd3c893c14b1ae8b78bd40485c715d962ad90"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="天网智联" title="天网智联"></a>
     </ul>
     
     <ul>
-	<h2>本网站技术支持由天网科技提供</h2>
+	<h2>本网站技术支持由天网智联提供</h2>
     </ul>
 
   </div>
